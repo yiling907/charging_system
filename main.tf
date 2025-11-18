bd0c4b05259759c7318ac9694a363e5cdee61670
yiling <yilinglei907@gmail.com>
2025-11-18 11:21:05 +0000

    fix: fix response pattern

--- main.tf
+++ main.tf
@@ -158,7 +158,7 @@ resource "aws_api_gateway_method" "get_image" {
   authorization = "NONE"
 
   request_parameters = {
-    "method.request.path.user_id"  = true
+    "method.request.path.user_id"    = true
     "method.request.path.image_name" = true
   }
 }
@@ -169,17 +169,6 @@ resource "aws_api_gateway_method_response" "response_200" {
   resource_id = aws_api_gateway_resource.image_name.id
   http_method = aws_api_gateway_method.get_image.http_method
   status_code = "200"
-
-  response_parameters = {
-    "method.response.header.Content-Type" = true
-  }
-}
-
-resource "aws_api_gateway_method_response" "response_404" {
-  rest_api_id = aws_api_gateway_rest_api.gateway.id
-  resource_id = aws_api_gateway_resource.image_name.id
-  http_method = aws_api_gateway_method.get_image.http_method
-  status_code = "404"
 
   response_parameters = {
     "method.response.header.Content-Type" = true
@@ -205,10 +194,11 @@ resource "aws_api_gateway_integration" "s3_integration" {
 
 
 resource "aws_api_gateway_integration_response" "s3_response" {
-  rest_api_id = aws_api_gateway_rest_api.gateway.id
-  resource_id = aws_api_gateway_resource.image_name.id
-  http_method = aws_api_gateway_method.get_image.http_method
-  status_code = aws_api_gateway_method_response.response_200.status_code
+  rest_api_id       = aws_api_gateway_rest_api.gateway.id
+  resource_id       = aws_api_gateway_resource.image_name.id
+  http_method       = aws_api_gateway_method.get_image.http_method
+  status_code       = aws_api_gateway_method_response.response_200.status_code
+  selection_pattern = aws_api_gateway_method_response.response_200.status_code
   response_parameters = {
     "method.response.header.Content-Type" = "integration.response.header.Content-Type"
   }
