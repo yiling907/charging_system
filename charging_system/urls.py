4cf0643e3b4d37e257ea5948645b207c2231d98c
yiling <yilinglei907@gmail.com>
2025-11-20 09:20:58 +0000

    refactor: remove unused urls

--- charging_system/urls.py
+++ charging_system/urls.py
@@ -17,13 +17,10 @@ schema_view = get_schema_view(
         license=openapi.License(name="BSD License"),
     ),
     public=True,
-    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
-    path('swagger.<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('api/charging/', include('charging.urls')),
 ]
