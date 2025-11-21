a8d73efa4be5a9ca64bfb2251d26d835fa1a7d0a
yiling <yilinglei907@gmail.com>
2025-11-21 19:54:43 +0000

    config:updates for eb

--- backend/charging/management/commands/createsu.py
+++ backend/charging/management/commands/createsu.py
@@ -1,5 +1,7 @@
-from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class Command(BaseCommand):
